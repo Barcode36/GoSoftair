3eb1f84172cf821f46c12431426561f1efdbe65e
DavidGia <gia.davide@gmail.com>
2015-09-20 18:48:50 +0200

    funziona ma è lenta
    ho creato la funzione connetti() che viene richiamata dai costruttori di
    tutte le classi in foundation che praticamente era il contenuto del
    costruttore di fdb

---
+++ New softair/Foundation funzionante/Foundation/FAnnuncio.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @access public
+ * @package Foundation
+ */
+class FAnnuncio extends Fdb {
+    public function __construct() {
+        $this->_table='annuncio';
+        $this->_key='IDannuncio';
+        $this->_return_class='EAnnuncio';
+        USingleton::getInstance('Fdb');
+        $this->connessione();
+    }
+    /**
+     * Funzione che fa lo store di un annuncio nella tabella
+     *
+     * @param EAnnuncio $annuncio
+     *
+     */
+    /*public function store( $annuncio) {
+    	parent::store($annuncio);
+    
+    }*/
+    /**
+     * Funzione che cancella un annuncio dalla tabella
+     *
+     * @param string $idannuncio
+     *
+     */
+    /*public function delete( & $idannuncio) {
+    	parent::delete($idannuncio);
+    }*/
+    
+    
+    /**
+     * Seleziona sul database gli annunci di un utente
+     *
+     * @param string $username
+     * @return array
+     */
+   /* public function getAnnunciUtente($username){
+    	$query='SELECT * ' .
+    			'FROM `annuncio` '
+    			'WHERE `autore=$username`';
+    	$this->query($query);
+    	return $this->getResultAssoc();
+    }*/
+    //c'� qualche errore nella query'
+	
+
+    public function loadall() {
+    	$query='SELECT * ' .
+    			'FROM `'.$this->_table.'` ORDER BY `annuncio`.`data` ASC';
+    	$this->query($query);
+    	return $this->getObjectArray();
+    }
+    
+	public function loadfromuser($key) {
+    	$query='SELECT * ' .
+    			'FROM `'.$this->_table.'` ' .
+    			'WHERE autoreusername = \''.$key.'\'';
+    	$this->query($query); 
+    	return $this->getObjectArray();
+    }
+    
+    public function deleterel($annunci){
+    	for($i=0; $i<count($annunci); $i++)
+    		$this->delete($annunci[$i]);
+    	
+    }
+	
+}
+
+?>
